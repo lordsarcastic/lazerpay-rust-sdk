a354c7a791d4662330c0182484821a492e266983
Lord-sarcastic <adeoti.15.jude@gmail.com>
2022-02-19 22:57:58 +0100

    feature: implemented check for api key existence

---
+++ src/constants.rs
@@ -0,0 +1,3 @@
+pub const API_KEY_ENV_VAR: &str = "API_KEY";
+pub const API_KEY_HEADER_KEY: &str = "x-api-key";
+pub const API_BASE_URL: &str = "https://api.lazerpay.engineering/api/v1/";
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod utils;
+mod constants;
+
 fn main() {
     println!("And it begins...");
+    if let Err(msg) = utils::validate_env_presence() {
+        panic!("{}", msg);
+    };
 }
